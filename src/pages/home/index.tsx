c1818929fdcecd31c513cf8d480b9afd59da9a2e
udhayasilambam96 <udhayasilambam96@hotmail.com>
2024-04-25 23:42:46 +0530

    mobile view fixed

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,78 +1,88 @@
 import React from "react";
 import useFetch from "react-fetch-hook";
 import styled from "styled-components";
-import './../../assets/home.css';
-
+import "./../../assets/home.css";
 
 import SliderPage from "../slider";
 
-const Wrapper = styled.div`
-`;
+const Wrapper = styled.div``;
 
 const LoginPage: React.FC = () => {
-  const { data, isLoading, error }: any = useFetch('https://krds-assignment.github.io/aoc/api-assets/data.json')
+  const { data, isLoading, error }: any = useFetch(
+    "https://krds-assignment.github.io/aoc/api-assets/data.json"
+  );
 
   if (error) {
-    console.log(error)
+    console.log(error);
   }
   console.log({ data });
   const contentStyle: React.CSSProperties = {
-    height: '160px',
-    color: '#fff',
-    lineHeight: '160px',
-    textAlign: 'center',
-    background: '#364d79',
+    height: "160px",
+    color: "#fff",
+    lineHeight: "160px",
+    textAlign: "center",
+    background: "#364d79",
   };
   return (
     <Wrapper>
       <div>
         {isLoading && <div>Loading...</div>}
-        {data && data.features.length > 0 && <div className="container">
-          {data.features.map((item: any) => (
-            <div
-              style={{
-                background: item?.title?.includes("Astonishing") ? "lightgreen"
-                  : item?.title?.includes("Be hooked") ? "coral"
-                    : item?.title?.includes("best things in life") ? "slateblue"
-                      : item?.title?.includes("thump never ends") ? "darkorange"
-                        : item?.title?.includes("Being globally you") ? "indianred"
-                          : item?.title?.includes("For the wise") && "skyblue"
-              }}
-            >
-              {item?.title?.includes("Astonishing") && (
-                <div style={{textAlign:"justify",width:"100%",marginLeft:"20px"}}>
-                  <img className="logo" src="https://krds-assignment.github.io/aoc/api-assets/logo-aoc.png" />
-                </div>
-              )}
+        {data && data.features.length > 0 && (
+          <div className="container desktop-view">
+            {data.features.map((item: any) => (
               <div
-                className="brand-container"
+                style={{
+                  background: item?.title?.includes("Astonishing")
+                    ? "lightgreen"
+                    : item?.title?.includes("Be hooked")
+                    ? "coral"
+                    : item?.title?.includes("best things in life")
+                    ? "slateblue"
+                    : item?.title?.includes("thump never ends")
+                    ? "darkorange"
+                    : item?.title?.includes("Being globally you")
+                    ? "indianred"
+                    : item?.title?.includes("For the wise") && "skyblue",
+                }}
               >
-                <div className="child-container">
-                  <div className="content">
-                    <div>
-                      <img src={item?.logo} />
-                    </div>
-                    <div className="title">
-                      {item?.title}
-                    </div>
-                    <div style={{color:"#FFF"}}>
-                      {item?.desc}
-                    </div>
+                {item?.title?.includes("Astonishing") && (
+                  <div
+                    style={{
+                      textAlign: "justify",
+                      width: "100%",
+                      marginLeft: "20px",
+                    }}
+                  >
+                    <img
+                      className="logo"
+                      src="https://krds-assignment.github.io/aoc/api-assets/logo-aoc.png"
+                    />
                   </div>
-                  <div>
-                    <img className="imageC" src={item?.image} />
+                )}
+                <div className="brand-container">
+                  <div className="child-container">
+                    <div className="content">
+                      <div>
+                        <img src={item?.logo} />
+                      </div>
+                      <div className="title">{item?.title}</div>
+                      <div style={{ color: "#FFF" }}>{item?.desc}</div>
+                    </div>
+                    <div>
+                      <img className="imageC" src={item?.image} />
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          ))}
-        </div>}
+            ))}
+          </div>
+        )}
         <div className="Carousel-container">
-          {/* <SliderPage /> */}
+          <SliderPage />
         </div>
       </div>
     </Wrapper>
-  )
+  );
 };
 
 export default LoginPage;
--- src/pages/slider/index.tsx
+++ src/pages/slider/index.tsx
@@ -1,81 +1,84 @@
 import React from "react";
 import useFetch from "react-fetch-hook";
 import styled from "styled-components";
-import './../../assets/slider.css';
-
-const Wrapper = styled.div`
-`;
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./../../assets/slider.css";
+import { IoIosArrowForward, IoIosArrowBack } from "react-icons/io";
+const Wrapper = styled.div``;
 
 const LoginPage: React.FC = () => {
-    const { data, isLoading, error }: any = useFetch('https://krds-assignment.github.io/aoc/api-assets/data.json')
+  const { data, isLoading, error }: any = useFetch(
+    "https://krds-assignment.github.io/aoc/api-assets/data.json"
+  );
 
-    if (error) {
-        console.log(error)
-    }
-    console.log({ data });
-    return (
-        <Wrapper><div>
-            <div className="carousel">
-                <ul className="slides">
-                    {isLoading && <div>Loading...</div>}
-                    {data && data.features.length > 0 && <div className="container">
-                        {data.features.map((item: any, index: any) => (
-                            <>
-                                <input type="radio" name="radio-buttons" id={`img-${index + 1}`} checked />
-                                <li className="slide-container">
-                                    <div
-                                        className="brand-container"
-                                        style={{
-                                            background: item?.title?.includes("Astonishing") ? "lightgreen"
-                                                : item?.title?.includes("Be hooked") ? "coral"
-                                                    : item?.title?.includes("best things in life") ? "slateblue"
-                                                        : item?.title?.includes("thump never ends") ? "darkorange"
-                                                            : item?.title?.includes("Being globally you") ? "indianred"
-                                                                : item?.title?.includes("For the wise") && "skyblue"
-                                        }}
-                                    >
-                                        <div className="child-container">
-                                            <div className="content">
-                                                <div>
-                                                    <img src={item?.logo} />
-                                                </div>
-                                                <div className="title">
-                                                    {item?.title}
-                                                </div>
-                                                <div>
-                                                    {item?.desc}
-                                                </div>
-                                            </div>
-                                            <div>
-                                                <img className="imageC" src={item?.image} />
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="carousel-controls">
-                                        <label htmlFor={`img-${index - 1}`} className="prev-slide">
-                                            <span>&lsaquo;</span>
-                                        </label>
-                                        <label htmlFor={`img-${index + 1}`} className="next-slide">
-                                            <span>&rsaquo;</span>
-                                        </label>
-                                    </div>
-                                </li>
-                            </>
-                        ))}
-                    </div>}
-                    <div className="carousel-dots">
-                        <label htmlFor="img-1" className="carousel-dot" id="img-dot-1"></label>
-                        <label htmlFor="img-2" className="carousel-dot" id="img-dot-2"></label>
-                        <label htmlFor="img-3" className="carousel-dot" id="img-dot-3"></label>
-                        <label htmlFor="img-4" className="carousel-dot" id="img-dot-4"></label>
-                        <label htmlFor="img-5" className="carousel-dot" id="img-dot-5"></label>
-                        <label htmlFor="img-6" className="carousel-dot" id="img-dot-6"></label>
-                    </div>
-                </ul>
-            </div>
+  if (error) {
+    console.log(error);
+  }
+  console.log({ data });
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 1500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: <IoIosArrowForward />,
+    prevArrow: <IoIosArrowBack />,
+  };
+  return (
+    <Wrapper>
+      <div>
+        <div className="carousel">
+          <ul className="slides">
+            {isLoading && <div>Loading...</div>}
+            {data && data.features.length > 0 && (
+              <div className="container">
+                <Slider {...settings}>
+                  {data.features.map((item: any, index: any) => (
+                    <>
+                      <li className="slide-container">
+                        <div
+                          className="brand-container"
+                          style={{
+                            background: item?.title?.includes("Astonishing")
+                              ? "lightgreen"
+                              : item?.title?.includes("Be hooked")
+                              ? "coral"
+                              : item?.title?.includes("best things in life")
+                              ? "slateblue"
+                              : item?.title?.includes("thump never ends")
+                              ? "darkorange"
+                              : item?.title?.includes("Being globally you")
+                              ? "indianred"
+                              : item?.title?.includes("For the wise") &&
+                                "skyblue",
+                          }}
+                        >
+                          <div className="child-container slider-section">
+                            <div className="content">
+                              <div>
+                                <img src={item?.logo} />
+                              </div>
+                              <div className="title">{item?.title}</div>
+                              <div>{item?.desc}</div>
+                            </div>
+                            <div>
+                              <img className="imageC" src={item?.image} />
+                            </div>
+                          </div>
+                        </div>
+                      </li>
+                    </>
+                  ))}
+                </Slider>
+              </div>
+            )}
+          </ul>
         </div>
-        </Wrapper>
-    )
+      </div>
+    </Wrapper>
+  );
 };
 
 export default LoginPage;
